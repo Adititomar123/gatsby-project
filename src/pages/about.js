9727599de303e6f2ec68acfc127dfc43090d52b1
Aditi Tomar <adititomar2k8@gmail.com>
2024-12-02 18:32:00 +0530

    test

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,9 +11,9 @@ function About() {
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
         <div className={styles.wrapper}>
          <picture>
-          <source type="image/webp" srcSet="https://images.ctfassets.net/8fjb1swihtib/4qpK9c95PhnngAiAHBQHi4/dcb57fa2b24bfad515105801f9e8a7ec/Tile_740x740_Netflix.png?w=414&amp;fm=webp 414w, https://images.ctfassets.net/8fjb1swihtib/4qpK9c95PhnngAiAHBQHi4/dcb57fa2b24bfad515105801f9e8a7ec/Tile_740x740_Netflix.png?w=828&amp;fm=webp 828w,"  sizes="100vw"/>
+          {/* <source type="image/webp" srcSet="https://images.ctfassets.net/8fjb1swihtib/4qpK9c95PhnngAiAHBQHi4/dcb57fa2b24bfad515105801f9e8a7ec/Tile_740x740_Netflix.png?w=414&amp;fm=webp 414w, https://images.ctfassets.net/8fjb1swihtib/4qpK9c95PhnngAiAHBQHi4/dcb57fa2b24bfad515105801f9e8a7ec/Tile_740x740_Netflix.png?w=828&amp;fm=webp 828w,"  sizes="100vw"/> */}
 
-          <img alt="big banner" loading="eager" width="414" height="414" srcSet="https://images.ctfassets.net/8fjb1swihtib/4qpK9c95PhnngAiAHBQHi4/dcb57fa2b24bfad515105801f9e8a7ec/Tile_740x740_Netflix.png?w=414 414w, https://images.ctfassets.net/8fjb1swihtib/4qpK9c95PhnngAiAHBQHi4/dcb57fa2b24bfad515105801f9e8a7ec/Tile_740x740_Netflix.png?w=828 828w," sizes="100vw" className={styles.image}/>
+          <img alt="big banner" loading="eager" width="414" height="414" srcSet="https://images.ctfassets.net/8fjb1swihtib/4qpK9c95PhnngAiAHBQHi4/dcb57fa2b24bfad515105801f9e8a7ec/Tile_740x740_Netflix.png?w=414," sizes="100vw" className={styles.image}/>
       
          </picture>
         </div>
